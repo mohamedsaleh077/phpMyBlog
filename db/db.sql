1eefe06225a5aa4b30b200112d361304366585ab
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-02-21 19:22:30 +0200

    update db

--- db/db.sql
+++ db/db.sql
@@ -1,13 +1,32 @@
+CREATE TABLE IF NOT EXISTS `categories` (
+                                            `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+                                            `name` VARCHAR(255) NOT NULL,
+    `info` TEXT,
+    PRIMARY KEY(`id`)
+    );
+
+CREATE TABLE IF NOT EXISTS `admins` (
+                                        `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+                                        `username` VARCHAR(255) NOT NULL UNIQUE,
+    `fullname` VARCHAR(255),
+    `info` TEXT,
+    `pfp` VARCHAR(255),
+    `pwd_hash` VARCHAR(255) NOT NULL,
+    `2fa`   VARCHAR(255),
+    PRIMARY KEY(`id`)
+    );
+
+
 CREATE TABLE IF NOT EXISTS `articles` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    `author_id` INTEGER NOT NULL,
-    `category_id` INTEGER NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
+                                          `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+                                          `author_id` INTEGER NOT NULL,
+                                          `category_id` INTEGER NOT NULL,
+                                          `title` VARCHAR(255) NOT NULL,
     `slug` VARCHAR(255) NOT NULL UNIQUE,
-    `content` MEDIUMTEXT(65535) NOT NULL,
+    `content` TEXT NOT NULL,
     `keywords` TEXT NOT NULL,
     `seo_title` VARCHAR(255) NOT NULL,
-    `meta_description` TEXT(65535) NOT NULL,
+    `meta_description` TEXT NOT NULL,
     `thumbnail` VARCHAR(255) NOT NULL,
     PRIMARY KEY(`id`),
 
@@ -16,24 +35,8 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
     FOREIGN KEY(`author_id`) REFERENCES `admins`(`id`)
     ON UPDATE NO ACTION ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS `categories` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    `name` VARCHAR(255) NOT NULL,
-    `info` TEXT,
-    PRIMARY KEY(`id`)
-);
-CREATE TABLE IF NOT EXISTS `admins` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-    `fullname` VARCHAR(255) NOT NULL,
-    `info` TEXT,
-    `pfp` VARCHAR(255),
-    `pwd_hash` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
-);
+    );
 
-CREATE INDEX `articles_index_0`
-    ON `articles` (`tags`);
 CREATE INDEX `articles_index_1`
     ON `articles` (`title`);
 CREATE INDEX `articles_index_2`
